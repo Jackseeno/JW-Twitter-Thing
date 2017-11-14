f21f16e0c018ef47185bb45f6413259ec1dd3fe4
Jackseeno <Jackseeno@users.noreply.github.com>
2017-11-14 17:17:12 -0500

    Update johnnygit.py

--- johnnygit.py
+++ johnnygit.py
@@ -3,38 +3,52 @@ import cPickle as pickle
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
+from datetime import date, timedelta, datetime
 
 def twitstuff():
 
 
-    a = 'xxx'
-    b = 'xxx'
-    c = 'xxx'
-    d = 'xxx'
+    a = 'zzz'
+    b = 'zzz'
+    c = 'zzz'
+    d = 'zzz'
     t = Twitter(auth=OAuth(a, b, c, d))
+    #ogid = 930143044749414401
+    
+    with open('id.txt', 'r+') as read_id:
+        last_id = int(read_id.readline())
+    print last_id
     jw = t.statuses.user_timeline(count=200, screen_name='JWONDER21')
+    it_id = jw[0]['id']
+    add_id = open('id.txt', 'w')
+    add_id.write(str(it_id))
+    add_id.close
     lst = []
     it = '#ITUNES'
     ht = 'http'
     remove = '-'
-    id = jw[0]['id']
     rtwd = 'Retweeted'
     tc = 'https://t.co'
     cl = 'click'
     new = "#new"
-    print id
+    you = 'youtu'
+    nuum = you[-2]
     for item in jw:
+        #print item['id']
         rt = item['retweeted']
         x = item['text'].encode('utf-8')
+
         if x [0:2] == 'RT': 
-            pass
+            pass #removes retweets
         elif x[:4] == cl:
-            pass
+            pass #removes click to buy
         elif x[:3] == new:
-            pass
+            pass #removes new music friday stuff
         elif x[0] == '@':
-            pass
+            pass #removes tweets that start with @
+
         elif ht in x:
+            
             end1 = int(x.find(it))
             end2 = int(x.find(ht))
             n = x[:end1]
@@ -42,23 +56,28 @@ def twitstuff():
             i = i.strip(' ')
             if i in lst:
                 pass
-            elif tc in i:
-                pass
+#            elif tc in i:
+#                pass
             else:
                 lst.append(i)
     return lst
-
-
+    print "Retrieved tweets..."
+#    wrte = open('id.txt', 'w')
+#    wrte.write(last_id)
+#    wrte.close()
+    
+    
 
 def ytsrch(x):
+    
     batch = {}
     url_header= "https://www.youtube.com/watch?v="
-    DEVELOPER_KEY = "xxx"
+    DEVELOPER_KEY = "zzz"
     YOUTUBE_API_SERVICE_NAME = "youtube"
     YOUTUBE_API_VERSION = "v3"
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
     page_size = 1
-    playlist = 'xxx'
+    playlist = 'PLGo8zA-BDOfTRjVNmjJA9CbpoiPaqzThT'
     for item in x:
         dd = item
         #print dd
@@ -73,22 +92,32 @@ def ytsrch(x):
                     vid_id = thing["id"]["videoId"].encode('utf-8')
                     url = url_header + vid_id
                     batch[dd] = url
-                    
+                    print "Added To Batch"
                 except KeyError:
                     continue
-    print "returning batch"
+    print "Youtube Search Complete"
     return batch
 
 def save_to_text(x):
+    print "Saving html file..."
     one = '<a href="'
     two = '" target="_blank">'
     three = '</a>'
-    f = open("jw.html", "w")
+    f = open("jw.html", "a")
+    datentime = str(datetime.now())
+    print datentime
+    f.write(datentime + '<br />' + '\n')
+    f.close()
     for key, value in x.items():
+        d = open("jw.html", "r+")
+        w = d.read()
         thing = one + value + two + key + three
         #check = f.readlines()
-        f.write(str(thing + '<br />'))
-        
+        if thing in w:
+            pass
+        else:
+            d.write(str(thing + '<br />' + '\n'))
+        d.close()
     f.close()
    
 ccc = twitstuff()
